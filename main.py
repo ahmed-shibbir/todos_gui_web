2537dc29eed76103b7d4b023b52e0b491bd54858
shibbir <shibbir63@yahoo.com>
2023-08-28 19:46:41 +0100

    About page added.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import streamlit as st
 import functions
 
 
+
+st.set_page_config(layout="wide")
+
 st.title("My To Do App")
 st.subheader("This is my to do app.")
-st.write("This app is to regularly update daily todos list.")
+st.write("<h1>This app is to regularly update <b>daily todos list</b>.</h1>",
+         unsafe_allow_html=True)
 
 
 # Populating todos list from the existing todos items
 todos = functions.get_todos()
-
 
 
 # Function to add an item to the todos list
@@ -24,6 +27,10 @@ def add_todo():
 # st.checkbox("Orange")
 # st.checkbox("Cherry")
 
+# Adding an item to the todos list
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
+
 # Creasing checkbox list with items in the todos list
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -34,9 +41,7 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-# Adding an item to the todos list
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
+
 
 # Session state display for convenience of coding(to be removed later)
 # st.session_state
